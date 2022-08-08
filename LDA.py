9d3a163a9c09285bb3700b12e3422a2aa8745b46
Adel Youssef <63298005+AdelYoussef@users.noreply.github.com>
2022-08-08 21:52:35 +0200

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -11,6 +11,15 @@ np.random.seed(400)
 import nltk
 
 
+
+newsgroups_train = fetch_20newsgroups(subset='train', shuffle = True)
+newsgroups_test = fetch_20newsgroups(subset='test', shuffle = True)
+
+nltk.download('omw-1.4')
+nltk.download('wordnet')
+stemmer = SnowballStemmer("english")
+
+
 class LDA :
         
         
@@ -47,9 +56,9 @@ class LDA :
         return processed_docs 
 
     
-    def doc2bow_dict(self, data):
+    def doc2bow_dict(self,data):
         self.dictionary = gensim.corpora.Dictionary(data)
-        self.bow_corpus = [self.dictionary.doc2bow(self.data_train) for doc in self.data_train]
+        self.bow_corpus = [self.dictionary.doc2bow(doc) for doc in data]
         return self.bow_corpus, self.dictionary
     
 
@@ -72,11 +81,14 @@ class LDA :
             
         return self.lda_model
         
+    def save_model(self,path):
+      path = path + "LDA_model"
+      self.lda_model.save(path)
 
-    def train(self,Filter = True , multicore = True , cores = 2 , batch_size = 10 , epochs = 50 ,  num_topics=10):
+    def train(self, path = "", save_model = True , Filter = True , multicore = True , cores = 2 , batch_size = 10 , epochs = 50 ,  num_topics=10 ):
         self.multicore = multicore
         self.Filter = Filter
-        self.processed_docs = self.preprocess(self.newsgroups_train)
+        self.processed_docs = self.preprocess(self.data_train)
         
         self.doc2bow_dict(self.processed_docs)
         
@@ -85,17 +97,18 @@ class LDA :
             self.filter_docs(min_reps = 15, min_occr = 0.1, keep_most_freq = None)
         
         
-        self.LDA_model(num_topics = num_topics, dictionary = self.dictionary, epochs = epochs)
+        self.LDA_model(num_topics = num_topics, dic = self.dictionary, epochs = epochs)
         print("training successful \n")
-            
 
+        if save_model == True:
+            self.save_model(path)
         
     def classify(self,data):
         predict = []
         for doc in (data):
           processed = self.parsing(doc)
           bow_vector = self.dictionary.doc2bow(processed)
-          vector =  np.array(self.LDA_model[bow_vector])
+          vector =  np.array(self.lda_model[bow_vector])
           vec = np.max(vector, axis=0)
           predict.append(vec)
           
@@ -108,7 +121,8 @@ test = fetch_20newsgroups(subset='test', shuffle = True)
   
 mylda = LDA()
 
-mylda.init(train.data , test.data)
+mytrain , mytest = mylda.init(train.data , test.data)
+
 
 mylda.train(Filter = False , multicore = False , batch_size = 10 , epochs = 50 ,num_topics=10)
 
@@ -118,9 +132,5 @@ unseen_document.append(newsgroups_test.data[100])
 unseen_document.append(newsgroups_test.data[50])
 unseen_document.append(newsgroups_test.data[60])
 
-
 predictions = mylda.classify(unseen_document)
 
-
-
-    
